4e0bf90d41b3b8919016e9197e3893af39691639
Mennah Jafar <mmohamed@talentscript.com>
2020-11-30 11:53:45 +0300

    firebase fnctions

--- src/components/firebase/functions/index.js
+++ src/components/firebase/functions/index.js
@@ -12,7 +12,7 @@ var transporter = nodemailer.createTransport({
     secure: true,
     auth: {
         user: 'mennahjafar@gmail.com',
-        pass: 'skyfallM'
+        pass: '*********'
     }
 });
 
